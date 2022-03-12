fbc7f518fc2948583a8d5812b18a81a0c3143f84
edigonzales <edi.gonzales@gmail.com>
2022-03-12 16:51:36 +0100

    hints

--- app/src/main/java/ch/so/agi/oereb/cts/App.java
+++ app/src/main/java/ch/so/agi/oereb/cts/App.java
@@ -1,5 +1,7 @@
 package ch.so.agi.oereb.cts;
 
+import com.occamlab.te.Test;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
@@ -8,10 +10,11 @@ public class App {
         
         
         // TODO:
-        // -source -> oereb ctl
+        // -source -> oereb ctl. Falls keine -source gesetzt wird, wird "../te_base/" verwendet. Das ist das Verzeichnis, wo die CTL drinstecken in der Distribution.
+        // gradle run überschreibt das. Ebenso kann das der Benutzer
         // - falls kein output, user.home verwenden.
         
-        
+        // Siehe SetupOptions.java
         System.setProperty("TE_BASE", "/Users/stefan/tmp/");
 
         Test test = new Test();
